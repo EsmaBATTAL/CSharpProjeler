4e6dc39cbdf29b872081d86dea404d3ad8489953
Esma BATTAL <106597738+EsmaBATTAL@users.noreply.github.com>
2025-12-23 17:22:09 +0300

    karakterDegistirme

---
+++ KarakterDegistirme/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace KarakterDegistirme
+{
+    class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("İfadeyi giriniz (kelimeler arası boşluk bırakın):");
+        string giris = Console.ReadLine();
+
+        string[] kelimeler = giris.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var kelime in kelimeler)
+        {
+            Console.Write(Degistir(kelime) + " ");
+        }
+    }
+
+    
+    static string Degistir(string kelime)
+    {
+        if (string.IsNullOrEmpty(kelime) || kelime.Length == 1)
+            return kelime; 
+
+        char[] karakterler = kelime.ToCharArray();
+
+        char ilk = karakterler[0];
+        char son = karakterler[karakterler.Length - 1];
+
+        karakterler[0] = son;
+        karakterler[karakterler.Length - 1] = ilk;
+
+        return new string(karakterler);
+    }
+}
+}
+
+

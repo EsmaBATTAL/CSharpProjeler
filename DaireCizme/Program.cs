14919389f54c3dfecc503c77963488ab6d30caaf
Esma BATTAL <106597738+EsmaBATTAL@users.noreply.github.com>
2025-12-23 12:12:59 +0300

    daireCizme

---
+++ DaireCizme/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DaireCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yaricap = YariCapAl();
+            DaireCiz(yaricap);
+            
+            
+        }
+        public static int YariCapAl()
+        {
+            Console.Write ("Lütfen dairenin yarıçapını giriniz: ");
+            int yariCap;
+            while(!int.TryParse(Console.ReadLine(), out yariCap) || yariCap <= 0)
+            {
+                Console.Write("Geçersiz giriş.Pozitif tamsayı giriniz: ");
+            }
+            return yariCap;
+        }
+        public static void DaireCiz(int yariCap)
+        {
+            int cap = 2 * yariCap;
+            for (int y = 0; y <= cap; y++)
+            {
+                for(int x = 0; x <= cap; x++)
+                {
+                    int dx = x - yariCap;
+                    int dy = y - yariCap;
+                    double uzaklik = Math.Sqrt(dx * dx + dy * dy);
+
+                    if (Math.Abs(uzaklik -yariCap) < 0.5)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                    Console.WriteLine();
+            }
+        }
+    }
+}
